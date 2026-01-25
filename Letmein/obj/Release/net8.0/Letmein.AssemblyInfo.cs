4efcda03065ad417e7397d674bea628b4bb45c25
Yair Cohen <mysmallfish@gmail.com>
2026-01-25 02:33:48 +0200

    Fix RTL calendar nav arrows and toolbar order

--- Letmein/obj/Release/net8.0/Letmein.AssemblyInfo.cs
+++ Letmein/obj/Release/net8.0/Letmein.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Letmein")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f1d8a3331c61b254653d46987d5b9f3bf2287b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a056e6b69b065b605ed81ed7d80221933d835792")]
 [assembly: System.Reflection.AssemblyProductAttribute("Letmein")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Letmein")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
