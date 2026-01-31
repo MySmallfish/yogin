6d10b62163389ab6d587a3949e1d77cba4ff06f3
Yair Cohen <mysmallfish@gmail.com>
2026-01-31 21:06:24 +0200

    Fix AddInvoices designer using

--- Letmein/Migrations/20260131120000_AddInvoices.Designer.cs
+++ Letmein/Migrations/20260131120000_AddInvoices.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Letmein.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
